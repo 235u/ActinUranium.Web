c9203993bc1aa41b4bb3cbcab5925002f8df0434
legion-von-gadara <legion@235u.net>
2019-09-16 11:56:54 +0200

    Document security settings in the startup configuration

--- ActinUranium.Web/Extensions/ApplicationBuilderExtensions.cs
+++ ActinUranium.Web/Extensions/ApplicationBuilderExtensions.cs
@@ -39,6 +39,17 @@ namespace ActinUranium.Web.Extensions
             return app.UseStaticFiles(options);
         }
 
+        /// <summary>
+        /// Sets the <c>X-Content-Type-Options</c> response header to prevent XSS attacks.
+        /// </summary>
+        /// <param name="app">The <see cref="IApplicationBuilder"/>.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
+        /// <remarks><c>X-Content-Type-Options</c> is a non-standard HTTP header telling browsers not to load
+        /// scripts and stylesheets unless the server indicates the correct MIME type. Without this header, the browsers
+        /// can incorrectly detect files as scripts and stylesheets, leading to XSS attacks. As such, all sites must set
+        /// the <c>X-Content-Type-Options</c> header and the appropriate MIME types for files that they serve.</remarks>
+        /// <seealso href="https://infosec.mozilla.org/guidelines/web_security#x-content-type-options">Mozilla's Web
+        /// Security Guidelines, X-Content-Type-Options</seealso>
         public static IApplicationBuilder UseContentTypeOptions(this IApplicationBuilder app)
         {
             app.Use(async (context, next) =>
@@ -50,6 +61,16 @@ namespace ActinUranium.Web.Extensions
             return app;
         }
 
+        /// <summary>
+        /// Sets the Content Security Policy's <c>frame-ancestors</c> directive to prevent clickjacking attacks.
+        /// </summary>
+        /// <param name="app">The <see cref="IApplicationBuilder"/>.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
+        /// <remarks>The <c>frame-ancestors</c> directive, which is not yet supported by all major browsers, supercedes
+        /// the <c>X-Frame-Options</c> header.</remarks>
+        /// <seealso cref="UseFrameOptions(IApplicationBuilder)"/>
+        /// <seealso cref="https://infosec.mozilla.org/guidelines/web_security.html#content-security-policy">Mozilla's
+        /// Web Security Guidelines, Content Security Policy</seealso>
         public static IApplicationBuilder UseContentSecurityPolicy(this IApplicationBuilder app)
         {
             app.Use(async (context, next) =>
@@ -61,6 +82,14 @@ namespace ActinUranium.Web.Extensions
             return app;
         }
 
+        /// <summary>
+        /// Sets the <c>X-Frame-Options</c> response header to prevent clickjacking attacks.
+        /// </summary>
+        /// <param name="app">The <see cref="IApplicationBuilder"/>.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
+        /// <remarks><c>X-Frame-Options</c> is a HTTP header controlling site's framing within an iframe.</remarks>
+        /// <seealso href="https://infosec.mozilla.org/guidelines/web_security#x-frame-options">Mozilla's Web Security
+        /// Guidelines, X-Frame-Options</seealso>
         public static IApplicationBuilder UseFrameOptions(this IApplicationBuilder app)
         {
             app.Use(async (context, next) =>
