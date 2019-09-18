0e5dbaaa013097f83d5b5fbfb754c6f0a31274ea
legion-von-gadara <legion@235u.net>
2019-09-18 17:01:17 +0200

    Fix casing issues; replace magic strings with constants

--- ActinUranium.Web/SharedResources.cs
+++ ActinUranium.Web/SharedResources.cs
@@ -8,17 +8,24 @@ namespace ActinUranium.Web
         Justification = "Not supported by the framework")]
     public class SharedResources
     {
-        public const string About = nameof(About);
-        public const string Awards = nameof(Awards);
-        public const string AwardsMetaDescription = nameof(AwardsMetaDescription);
-        public const string AwardsMetaKeywords = nameof(AwardsMetaKeywords);
+        public const string Creations = nameof(Creations);
+        public const string CreationsIcon = "Creations icon";
         public const string Customers = nameof(Customers);
+        public const string CustomersIcon = "Customers icon";
+        public const string GitHubIcon = "GitHub icon";
         public const string Headlines = nameof(Headlines);
+        public const string HeadlinesIcon = "Headlines icon";
         public const string Home = nameof(Home);
-        public const string Projects = nameof(Projects);
-        public const string SourceCode = nameof(SourceCode);
-        public const string TermsAndConditions = nameof(TermsAndConditions);
-        public const string Privacy = nameof(Privacy);
+        public const string HomeIcon = "Home icon";
         public const string Imprint = nameof(Imprint);
+        public const string LoyalCustomers = "Loyal customers";
+        public const string Privacy = nameof(Privacy);
+        public const string RecentHeadlines = "Recent headlines";
+        public const string RecentlyReleasedCreations = "Recently released creations";
+        public const string SourceCode = "Source code";
+        public const string Terms = nameof(Terms);
+        public const string TermsAndConditions = "Terms and conditions";
+        public const string ViewAllCreations = "View all creations";
+        public const string ViewAllHeadlines = "View all headlines";
     }
 }
