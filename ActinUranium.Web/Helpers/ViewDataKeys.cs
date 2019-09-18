2fd521beffbe7c03549b26753b53189a6de8cb37
legion-von-gadara <legion@235u.net>
2019-09-18 17:23:50 +0200

    Simplify css; replace magic strings with constants

---
+++ ActinUranium.Web/Helpers/ViewDataKeys.cs
@@ -0,0 +1,8 @@
+﻿namespace ActinUranium.Web.Helpers
+{
+    public static class ViewDataKeys
+    {
+        public const string Title = nameof(Title);
+        public const string VerticallyCentered = nameof(VerticallyCentered);
+    }
+}
