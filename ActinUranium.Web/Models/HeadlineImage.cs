0ca2bb9925b1b8da36a20ecdb9c687a71f4a793d
legion-von-gadara <legion@235u.net>
2019-09-23 19:36:08 +0200

    Clean up

--- ActinUranium.Web/Models/HeadlineImage.cs
+++ ActinUranium.Web/Models/HeadlineImage.cs
@@ -24,57 +24,5 @@ namespace ActinUranium.Web.Models
 
         [Display(Name = "Anzeigereihenfolge")]
         public byte DisplayOrder { get; set; } = 1;
-
-        internal static void OnModelCreating(EntityTypeBuilder<HeadlineImage> typeBuilder)
-        {
-            typeBuilder.HasIndex(ci => new { ci.HeadlineSlug, ci.DisplayOrder })
-                .IsUnique();
-
-            typeBuilder.HasOne(ci => ci.Headline)
-                .WithMany(c => c.HeadlineImages)
-                .HasForeignKey(ci => ci.HeadlineSlug);
-        }
-
-        internal static void Seed(ApplicationDbContext dbContext)
-        {
-            var imageLottery = new WeightedLottery<bool>
-            {
-                { true, 11 },
-                { false, 5 }
-            };
-
-            var imageNumberLottery = new Int32Lottery(1, 17);
-
-            foreach (Headline headline in dbContext.Headlines)
-            {
-                bool doCreateImage = imageLottery.Pull();
-                if (doCreateImage)
-                {
-                    int imageNumber = imageNumberLottery.Pull();
-                    string fileName = string.Format(CultureInfo.InvariantCulture, "{0:00}.svg", imageNumber);
-                    Image image = CreateImage(fileName);
-                    dbContext.Images.Add(image);
-
-                    var headlineImage = new HeadlineImage()
-                    {
-                        ImageSource = image.Source,
-                        HeadlineSlug = headline.Slug
-                    };
-
-                    dbContext.HeadlineImages.Add(headlineImage);
-                }
-            }
-
-            dbContext.SaveChanges();
-        }
-
-        private static Image CreateImage(string fileName)
-        {
-            return new Image
-            {
-                Source = $"~/img/headlines/{fileName}",
-                AlternativeText = LoremIpsum.NextSentence(),
-            };
-        }
     }
 }
