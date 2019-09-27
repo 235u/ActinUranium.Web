05a29635f1ccc928722d1c061c880739a959f94d
legion-von-gadara <legion@235u.net>
2019-09-27 10:49:16 +0200

    Balance headline image distribution

--- ActinUranium.Web/Services/ApplicationDbInitializer.Headlines.cs
+++ ActinUranium.Web/Services/ApplicationDbInitializer.Headlines.cs
@@ -2,6 +2,7 @@
 using ActinUranium.Web.Models;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace ActinUranium.Web.Services
 {
@@ -18,28 +19,18 @@ namespace ActinUranium.Web.Services
             };
         }
 
-        private static WeightedLottery<Tag> CreateTagLottery(int headlineCount)
+        private static IReadOnlyCollection<Tag> CreateTags(int count)
         {
-            var tagLottery = new WeightedLottery<Tag>();
+            var tags = new List<Tag>();
 
-            const int TargetCount = 2;
-            string[] tagNames = CreateUniqueSet(TargetCount, () => LoremIpsum.NextWord(minLength: 7));            
-
-            for (int tagIndex = 0; tagIndex < TargetCount; tagIndex++)
+            string[] tagNames = CreateUniqueSet(count, () => LoremIpsum.NextWord(minLength: 7));
+            foreach (string name in tagNames)
             {
-                string name = tagNames[tagIndex];
                 Tag tag = CreateTag(name);
-                int tagWeight = headlineCount / TargetCount;
-                if (tagIndex == 0)
-                {
-                    // headline count: 11, first tag's weight: 6, second tag's weight: 5
-                    tagWeight += headlineCount % TargetCount;
-                }
-
-                tagLottery.Add(tag, tagWeight);
+                tags.Add(tag);
             }
 
-            return tagLottery;
+            return tags;
         }
 
         private static Tag CreateTag(string name)
@@ -51,41 +42,97 @@ namespace ActinUranium.Web.Services
             };
         }
 
-        private static Headline CreateHeadline(string title, Author author, Tag tag, List<HeadlineImage> images)
+        private static WeightedLottery<Tag> CreateTagLottery(IReadOnlyCollection<Tag> tags, int totalWeight)
+        {
+            var tagLottery = new WeightedLottery<Tag>();            
+
+            foreach (Tag tag in tags)
+            {
+                int tagWeight = totalWeight / tags.Count;
+                if (tag == tags.First())
+                {
+                    // total weight: 11, tag count: 2, first tag's weight: 6, second tag's weight: 5
+                    tagWeight += totalWeight % tags.Count;
+                }
+
+                tagLottery.Add(tag, tagWeight);
+            }
+
+            return tagLottery;
+        }
+
+        private static Headline CreateHeadline(HeadlineInfo headline)
         {
             return new Headline
             {
-                Slug = title.Slugify(),
-                Title = title,
+                Slug = headline.Title.Slugify(),
+                Title = headline.Title,
                 Lead = LoremIpsum.NextParagraph(minSentenceCount: 1, maxSentenceCount: 2),
                 Text = LoremIpsum.NextParagraph(minSentenceCount: 2, maxSentenceCount: 4),
                 ReleaseDate = ActinUraniumInfo.NextDate(),
-                Author = author,
-                Tag = tag,
-                HeadlineImages = images
+                Author = headline.Author,
+                Tag = headline.Tag,
+                HeadlineImages = headline.Images
             };
         }
 
         private void SeedHeadlines()
-        {
-            Author author = CreateAuthor();            
-            DirectoryInfo[] imageDirectories = GetDirectories("img/headlines");
-            int headlineCount = imageDirectories.Length;
-            WeightedLottery<Tag> tagLottery = CreateTagLottery(headlineCount);
-            string[] headlineTitles = CreateUniqueSet(
-                headlineCount, () => LoremIpsum.NextHeading(minWordCount: 2, maxWordCount: 8));            
-
-            for (int headlineIndex = 0; headlineIndex < headlineCount; headlineIndex++)
+        {            
+            IEnumerable<HeadlineInfo> headlineInfos = GetHeadlineInfos();           
+            foreach (var info in headlineInfos)
             {
-                string title = headlineTitles[headlineIndex];
-                Tag tag = tagLottery.Pull();
-                DirectoryInfo directory = imageDirectories[headlineIndex];
-                List<HeadlineImage> images = GetHeadlineImages(directory);
-                Headline headline = CreateHeadline(title, author, tag, images);
+                Headline headline = CreateHeadline(info);
                 _dbContext.Add(headline);
             }
 
             _dbContext.SaveChanges();
+        }
+
+        private IEnumerable<HeadlineInfo> GetHeadlineInfos()
+        {
+            var headlineInfos = new List<HeadlineInfo>();
+
+            Author author = CreateAuthor();
+            DirectoryInfo[] imageDirectories = GetDirectories("img/headlines");
+
+            int illustratedHeadlineCount = imageDirectories.Length;
+            int nonIllustratedHeadlineCount = illustratedHeadlineCount / 2;
+            int totalHeadlineCount = illustratedHeadlineCount + nonIllustratedHeadlineCount;
+
+            IReadOnlyCollection<Tag> tags = CreateTags(count: 2);
+            WeightedLottery<Tag> tagLottery = CreateTagLottery(tags, totalWeight: illustratedHeadlineCount);
+            string[] titles = CreateUniqueSet(
+                totalHeadlineCount, () => LoremIpsum.NextHeading(minWordCount: 2, maxWordCount: 8));            
+
+            for (int headlineIndex = 0; headlineIndex < illustratedHeadlineCount; headlineIndex++)
+            {
+                DirectoryInfo directory = imageDirectories[headlineIndex];
+                var headlineInfo = new HeadlineInfo
+                {
+                    Images = GetHeadlineImages(directory),
+                    Tag = tagLottery.Pull(),
+                    Title = titles[headlineIndex],
+                    Author = author
+                };
+
+                headlineInfos.Add(headlineInfo);
+            }
+
+            tagLottery = CreateTagLottery(tags, totalWeight: nonIllustratedHeadlineCount);
+            for (int headlineIndex = illustratedHeadlineCount; headlineIndex < totalHeadlineCount; headlineIndex++)
+            {
+                var headlineInfo = new HeadlineInfo
+                {
+                    Images = new List<HeadlineImage>(),
+                    Tag = tagLottery.Pull(),
+                    Title = titles[headlineIndex],
+                    Author = author
+                };
+
+                headlineInfos.Add(headlineInfo);
+            }
+
+            return headlineInfos;
         }
 
         private List<HeadlineImage> GetHeadlineImages(DirectoryInfo directory)
@@ -94,16 +141,27 @@ namespace ActinUranium.Web.Services
 
             IEnumerable<string> imageSources = GetImageSources(directory);
             foreach (string source in imageSources)
-            {                
+            {
                 var headlineImage = new HeadlineImage
                 {
-                    Image = CreateImage(source),                    
+                    Image = CreateImage(source),
                 };
 
                 headlineImages.Add(headlineImage);
             }
 
             return headlineImages;
+        }
+
+        private class HeadlineInfo
+        {
+            public List<HeadlineImage> Images { get; set; }
+
+            public Tag Tag { get; set; }
+
+            public string Title { get; set; }            
+
+            public Author Author { get; set; }
         }
     }
 }
